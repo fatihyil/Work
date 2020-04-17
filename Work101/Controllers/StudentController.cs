939cae3a347f0b6a29fc9abc71f2b84d58280708
fatih yılmaz <fatihksks_@hotmail.com>
2020-04-17 03:31:09 +0300

    Öğrenci Crud işlemleri tamamlandı.

--- Work101/Controllers/StudentController.cs
+++ Work101/Controllers/StudentController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Work101.Entities;
 using Work101.Models;
 using Work101.Service;
 
@@ -36,6 +37,53 @@ namespace Work101.Controllers
                 });
             }
             return View(studentList);
+        }
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(StudentVM model)
+        {
+            var studentEntity = new StudentEntity();
+            var studentEntityCount = _studentService.GetStudents().Count;
+            studentEntity.Id = studentEntityCount + 1;
+            studentEntity.FirtsName = model.FirtsName;
+            studentEntity.LastName = model.LastName;
+            studentEntity.StudentNo = model.StudentNo;
+            studentEntity.ClassNo = model.ClassNo;
+            studentEntity.Birthday = model.Birthday;
+            _studentService.Add(studentEntity);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(int id)
+        {
+            var studententity = _studentService.Get(id);
+            var model = new StudentVM();
+            model.Id = studententity.Id;
+            model.FirtsName = studententity.FirtsName;
+            model.LastName = studententity.LastName;
+            model.StudentNo = studententity.StudentNo;
+            model.ClassNo = studententity.ClassNo;
+            model.Birthday = studententity.Birthday;
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult Edit(StudentVM model)
+        {
+            var studententity = _studentService.Get(model.Id);
+            studententity.FirtsName = model.FirtsName;
+            studententity.LastName = model.LastName;
+            studententity.Birthday = model.Birthday;
+            studententity.ClassNo = model.ClassNo;
+            studententity.StudentNo = model.StudentNo;
+            _studentService.Edit(studententity);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int id)
+        {
+            _studentService.Delete(id);
+            return RedirectToAction("Index");
         }
     }
 }
--- Work101/Service/StudentService.cs
+++ Work101/Service/StudentService.cs
@@ -21,5 +21,29 @@ namespace Work101.Service
         {
             return _employeeContext.Students;
         }
+        //id ye göre istenilen öğrenici geri döndüren sınıf
+        public StudentEntity Get(int id)
+        {
+            return _employeeContext.Students.FirstOrDefault(x => x.Id == id);//id ye göre istenilen öğreniciyi return ediyoruz.
+        }
+        //id ye göre değişiklik yapılmak istenilen öğrenci veritanından önce siliniyor sonra yeni bilgileri veritabanına gönderiliyor.
+        public void  Edit(StudentEntity studentEntity)
+        {
+            var entity = _employeeContext.Students.FirstOrDefault(x => x.Id == studentEntity.Id);//Eski veriyi bir değişkene atıyoruz.
+            _employeeContext.Students.Remove(entity);//eski veri veritabanından siliniyor.
+            _employeeContext.Students.Add(studentEntity);//yeni veri ekleniyor.
+        }
+        //Silinmek istenilen öğrenci bilgileri id ye göre bu class'a gelir ve veritabanına gönderilir.
+        public void Delete(int id)
+        {
+            var entity = _employeeContext.Students.FirstOrDefault(x=>x.Id == id);
+            _employeeContext.Students.Remove(entity);
+        }
+
+        //Eklenmek istenilen öğrenci bu class'da veritabanına gönderilir.
+        public void Add(StudentEntity studentEntity)
+        {
+            _employeeContext.Students.Add(studentEntity);
+        }
     }
 }
